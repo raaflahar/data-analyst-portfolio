9715c1a976ef949f0f13a8179dce8262a7425f06
raaflahar <raaflahar@gmail.com>
2024-11-08 19:24:56 +0700

    Update portfolio

--- src/assets/Container/Portfolio/Portfolio.jsx
+++ src/assets/Container/Portfolio/Portfolio.jsx
@@ -31,7 +31,7 @@ export default function Portfolio() {
                       className=""
                     >
                       <img
-                        src="src/assets/img/portfolio/01-penjualan-smartphone.jpg"
+                        src="https://github.com/raaflahar/data-analyst-portfolio/blob/main/src/assets/img/portfolio/01-penjualan-smartphone.jpg?raw=true"
                         alt="Smartphone Sales"
                         className="w-full"
                       />
